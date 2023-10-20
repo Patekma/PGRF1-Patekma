ddcc2d03a2dec09d50eb2571e1e66449954764c9
Patek <matesptek@seznam.cz>
2023-10-20 14:00:33 +0200

    Trivial liner fixed for all angles

--- src/rasterops/LinerTrivial.java
+++ src/rasterops/LinerTrivial.java
@@ -9,22 +9,44 @@ public class LinerTrivial implements Liner{
     @Override
     public void drawLine(Raster img, double x1, double y1, double x2, double y2, int color) {
 
-        if(x2<x1){
-            double temp = x1;
+        double k = (y2 - y1) / (x2 - x1);
+        double q = y1 - k * x1;
+        double temp;
+
+        if(x2 < x1)
+        {
+            temp = x1;
             x1 = x2;
             x2 = temp;
+        }
 
+        if(y2 < y1)
+        {
             temp = y1;
             y1 = y2;
             y2 = temp;
         }
-        double k = (y2-y1) / (x2-x1);
-        double q = y1 - k * x1;
-        double y;
 
-        for (int x = (int) Math.round(x1); x < x2; x++) {
-            y = (k * x) + q;
-            img.setColor(x, (int) y,color);
+        if(x2 - x1 == 0)
+        {
+            for (double y = y1; y <= y2; y++) {
+                double x = x1;
+                img.setColor((int) x, (int) y, color);
+            }
         }
+        else if(Math.abs(k) <= 1) {
+            for (double x = x1; x <= x2; x++) {
+                int y = (int) Math.round(k * x + q);
+                img.setColor((int) x, (int) y, color);
+            }
+        }
+        else if(Math.abs(k) > 1)
+        {
+            for (double y = y1; y <= y2; y++) {
+                double x = (int) Math.round((y - q)/k);
+                img.setColor((int) x, (int) y, color);
+            }
+        }
+
     }
 }
